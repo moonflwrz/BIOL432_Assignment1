505ac9251a46fda872b53f31a69ad29912855303
Charlie W <122372970+moonflwrz@users.noreply.github.com>
2023-01-10 12:26:20 -0500

    Tidying up.

---
+++ dataGenerato.R
@@ -0,0 +1,40 @@
+#loading the dplyr package..
+library(dplyr)
+
+#setting up our organism names..
+namesVec<-c("Thylacinus cynocephalus",
+            "Raphus cucullatus",
+            "Hydrodamalis gigas",
+            "Equus quagga quagga",
+            "Ectopistes migratorius")
+#..then generating a random 100-size vector
+speciesNom<-sample(namesVec, 100, 
+                   replace=T)
+
+limb_width<-abs(rnorm(100, 3, 1))
+limb_length<-abs(rnorm(100, 6, 2))
+#random normal distribution
+  
+observ<-c("H. Mason",
+          "J. Sunderland",
+          "A. Gillespie")
+
+obserNom<-sample(observ, 100,
+                 replace=T)
+#..then adding observer names
+
+measurDat<-data.frame(speciesNom, limb_width,
+           limb_length, obserNom)
+#putting everything into a data.frame..
+
+measurDat<-measurDat %>% 
+  rename(Organism = speciesNom) %>% 
+  rename(Observer = obserNom)
+#then tidying it up..
+
+write.csv(measurDat,
+          "measurements.csv",
+          row.names=F)
+#..and exporting it as .CSV
+
+
